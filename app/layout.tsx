68b362d524b988f03657ea256a03fb56f223379c
SakuraHaruko <buguwu.official@outlook.com>
2024-02-08 17:51:53 +0800

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-
+  metadataBase: new URL('https://qwq.lgbt'),
 
   title: 'Short Url - NekoCafe',
   description: 'Shorten your URL~',
